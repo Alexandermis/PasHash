8ed74b8104c9d7e0c4273d0e0769e4ce0a384ba2
Alexandermis <73187089+Alexandermis@users.noreply.github.com>
2022-06-16 11:57:10 +0200

    salt

--- main.py
+++ main.py
@@ -24,8 +24,9 @@ def startVal(a,b):
         return b+a
 
 def salt(a):
-    b=""
-    return b
+    hash=calc_hash_list()
+    a=superHash(a[::-1])
+    return a[0:64]
 
 def order(a):
     x=0
@@ -33,25 +34,33 @@ def order(a):
         x += ord(c)
     return x
 
+def calc_hash_list():
+    hash = []
+    m1 = hashlib.sha256()
+    m2 = hashlib.sha3_512()
+    m3 = hashlib.sha3_384()
+    m4 = hashlib.sha3_256()
+    m5 = hashlib.blake2b()
+    m6 = hashlib.blake2s()
+    m7 = hashlib.md5()
+    hash.append(m1), hash.append(m2), hash.append(m3), hash.append(m4), hash.append(m5), hash.append(m6), hash.append(m7)
+    return  hash
 
+def superHash(startval):
+    hash = calc_hash_list()
+    while (len(hash) != 0):
+        x = order(startval)
+        hash[x % len(hash)].update(startval.encode())
+        startval = hash[x % len(hash)].hexdigest()
+        hash.remove(hash[x % len(hash)])
+    return startval
 if __name__ == '__main__':
     if len(sys.argv)!= 2:
         exit(-1)
     else:
         url=getUrl()
-        hash=[]
-        m1 = hashlib.sha256()
-        m2=hashlib.sha3_512()
-        m3=hashlib.sha3_384()
-        m4=hashlib.sha3_256()
-        m5=hashlib.blake2b()
-        m6=hashlib.blake2s()
-        hash.append(m1), hash.append(m2),hash.append(m3),hash.append(m4),hash.append(m5),hash.append(m6)
-        x=0
-        startval=startVal(url,sys.argv[1])
-        while(len(hash)!=0):
-            x = order(startval)
-            hash[x%len(hash)].update(startval.encode())
-            startval=hash[x%len(hash)].hexdigest()
-            hash.remove(hash[x%len(hash)])
-        print(startval)
+        startval = startVal(url, sys.argv[1])
+        startval=superHash(startval)
+        if len(startval)==64:
+            startval+=salt(startval)
+        print(f"Password.len: {len(startval)} Password: {startval}")
