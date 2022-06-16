91587abe7e1df71c830f994ab53ebe2ddbc81fb2
Alexandermis <73187089+Alexandermis@users.noreply.github.com>
2022-06-16 12:08:56 +0200

    Reformat code

--- main.py
+++ main.py
@@ -1,30 +1,35 @@
 import sys
 import hashlib
-#import webbrowser
 
-#from selenium import webdriver
-#from selenium.webdriver.firefox.options import Options
-#from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+# import webbrowser
+# from selenium import webdriver
+# from selenium.webdriver.firefox.options import Options
+# from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 def getUrl():
     return "https://www.spotify.com/"
 
-def startVal(a,b):
+
+def startVal(a, b):
     if ((len(a) + len(b)) % 2 == 0):
-        return a+b
+        return a + b
     else:
-        return b+a
+        return b + a
+
 
 def salt(a):
-    hash=calc_hash_list()
-    a=superHash(a[::-1])
+    hash = calc_hash_list()
+    a = superHash(a[::-1])
     return a[0:64]
 
+
 def order(a):
-    x=0
+    x = 0
     for c in a:
         x += ord(c)
     return x
+
 
 def calc_hash_list():
     hash = []
@@ -35,8 +40,10 @@ def calc_hash_list():
     m5 = hashlib.blake2b()
     m6 = hashlib.blake2s()
     m7 = hashlib.md5()
-    hash.append(m1), hash.append(m2), hash.append(m3), hash.append(m4), hash.append(m5), hash.append(m6), hash.append(m7)
-    return  hash
+    hash.append(m1), hash.append(m2), hash.append(m3), hash.append(m4), hash.append(m5), hash.append(m6), hash.append(
+        m7)
+    return hash
+
 
 def superHash(startval):
     hash = calc_hash_list()
@@ -46,17 +53,17 @@ def superHash(startval):
         startval = hash[x % len(hash)].hexdigest()
         hash.remove(hash[x % len(hash)])
     return startval
+
+
 if __name__ == '__main__':
-    if len(sys.argv)!= 2 and len(sys.argv)!=3:
+    if len(sys.argv) != 2 and len(sys.argv) != 3:
         exit(-1)
     else:
-        if len(sys.argv)==2:
+        if len(sys.argv) == 2:
             startval = startVal(getUrl(), sys.argv[1])
         else:
             startval = startVal(sys.argv[2], sys.argv[1])
-
-
-        startval=superHash(startval)
-        if len(startval)==64:
-            startval+=salt(startval)
+        startval = superHash(startval)
+        if len(startval) == 64:
+            startval += salt(startval)
         print(f"Password.len: {len(startval)} Password: {startval}")
